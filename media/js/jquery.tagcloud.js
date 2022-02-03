9275cbacb521f2f65910503294c58ddc5d6c52ba
xavier skip <xavierskip@gmail.com>
2022-02-03 19:56:46 +0800

    some meta

--- media/js/jquery.tagcloud.js
+++ media/js/jquery.tagcloud.js
@@ -82,7 +82,7 @@
 
 // config and run
 $.fn.tagcloud.defaults = {
-    size: {start: 7, end: 24, unit: "pt"},
+    size: {start: 10, end: 24, unit: "pt"},
       color: {start: '#f8e0e6', end: '#ff3333'}
 };
 $(function () {
